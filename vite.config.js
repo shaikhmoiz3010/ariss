1d56b719a3722a3a3ea7a07bd91d1e966e51f3a8
Shaikhmoiz3010 <Shaikhmoiz3010@gmail.com>
2026-03-30 16:10:51 +0530

    image name changed

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
